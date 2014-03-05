090b4ed02318ce10c8022efa6db309a08a0461b6
Matthew Weier O'Phinney <matthew@zend.com>
2014-03-05 11:32:35 -0600

    [#1] CS fixes
    - indentation, trailing whitespace

--- library/ZendXml/Security.php
+++ library/ZendXml/Security.php
@@ -13,7 +13,7 @@ use SimpleXMLElement;
 
 class Security
 {
-	const ENTITY_DETECT = 'Detected use of ENTITY in XML, disabled to prevent XXE/XEE attacks';
+    const ENTITY_DETECT = 'Detected use of ENTITY in XML, disabled to prevent XXE/XEE attacks';
 
     /**
      * Heuristic scan to detect entity in XML
@@ -29,7 +29,7 @@ class Security
     }
 
     /**
-     * Scan XML string for potential XXE and XEE attacks 
+     * Scan XML string for potential XXE and XEE attacks
      *
      * @param   string $xml
      * @param   DomDocument $dom
@@ -37,7 +37,7 @@ class Security
      * @return  SimpleXMLElement|DomDocument|boolean
      */
     public static function scan($xml, DOMDocument $dom = null)
-    {   	
+    {
         // If running with PHP-FPM we perform an heuristic scan
         // We cannot use libxml_disable_entity_loader because of this bug
         // @see https://bugs.php.net/bug.php?id=64938
@@ -48,11 +48,11 @@ class Security
         if (null === $dom) {
             $simpleXml = true;
             $dom = new DOMDocument();
-        } 
+        }
 
         if (!self::isPhpFpm()) {
-        	$loadEntities = libxml_disable_entity_loader(true);
-        	$useInternalXmlErrors = libxml_use_internal_errors(true);
+            $loadEntities = libxml_disable_entity_loader(true);
+            $useInternalXmlErrors = libxml_use_internal_errors(true);
         }
 
         // Load XML with network access disabled (LIBXML_NONET)
@@ -69,31 +69,31 @@ class Security
         if (!$result) {
             // Entity load to previous setting
             if (!self::isPhpFpm()) {
-            	libxml_disable_entity_loader($loadEntities);
-            	libxml_use_internal_errors($useInternalXmlErrors);
+                libxml_disable_entity_loader($loadEntities);
+                libxml_use_internal_errors($useInternalXmlErrors);
             }
             return false;
         }
 
         // Scan for potential XEE attacks using ENTITY, if not PHP-FPM
         if (!self::isPhpFpm()) {
-	        foreach ($dom->childNodes as $child) {
-	            if ($child->nodeType === XML_DOCUMENT_TYPE_NODE) {
-	                if ($child->entities->length > 0) {
-	                    throw new Exception\RuntimeException(self::ENTITY_DETECT);
-	                }
-	            }
-	        }
+            foreach ($dom->childNodes as $child) {
+                if ($child->nodeType === XML_DOCUMENT_TYPE_NODE) {
+                    if ($child->entities->length > 0) {
+                        throw new Exception\RuntimeException(self::ENTITY_DETECT);
+                    }
+                }
+            }
         }
 
         // Entity load to previous setting
-        if (!self::isPhpFpm()) { 
-        	libxml_disable_entity_loader($loadEntities);
-        	libxml_use_internal_errors($useInternalXmlErrors);
+        if (!self::isPhpFpm()) {
+            libxml_disable_entity_loader($loadEntities);
+            libxml_use_internal_errors($useInternalXmlErrors);
         }
 
         if (isset($simpleXml)) {
-            $result = simplexml_import_dom($dom); 
+            $result = simplexml_import_dom($dom);
             if (!$result instanceof SimpleXMLElement) {
                 return false;
             }
@@ -119,17 +119,17 @@ class Security
         }
         return self::scan(file_get_contents($file), $dom);
     }
-    
+
     /**
      * Return true if PHP is running with PHP-FPM
-     * 
+     *
      * @return boolean
      */
     public static function isPhpFpm()
     {
-    	if (substr(php_sapi_name(), 0, 3) === 'fpm') {
-    		return true;
-    	}
-    	return false;
+        if (substr(php_sapi_name(), 0, 3) === 'fpm') {
+            return true;
+        }
+        return false;
     }
 }
